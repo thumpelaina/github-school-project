26a93eef920baf1e15b613d8f5d02fa550d68fad
thumpelaina <thumpelaina@gmx.com>
2025-03-08 20:18:14 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,7 @@
+import java.util.*; 
+public class SchoolProject {
+  public static void main(String[] args) {
+    int a = (int)(Math.random()*10);
+    System.out.println("Number generated is "+a);
+  }
+}
